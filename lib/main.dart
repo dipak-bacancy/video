49a1c24d68933adbd2e663ee9b7b0d274915977c
Dipak <dipak.prajapati@bacancy.com>
2021-03-01 16:20:46 +0530

    continues video playing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,253 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(MyApp());
+main() {
+  runApp(MaterialApp(
+    home: VideoPlayerDemo(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class VideoPlayerDemo extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _VideoPlayerDemoState createState() => _VideoPlayerDemoState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _VideoPlayerDemoState extends State<VideoPlayerDemo> {
+  int index = 0;
+  double _position = 0;
+  double _buffer = 0;
+  bool _lock = true;
+  Map<String, VideoPlayerController> _controllers = {};
+  Map<int, VoidCallback> _listeners = {};
+  Set<String> _urls = {
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#1',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#2',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#3',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#4',
+    // 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#5',
+    // 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#6',
+    // 'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4#7',
+  };
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+    if (_urls.length > 0) {
+      _initController(0).then((_) {
+        _playController(0);
+      });
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+    if (_urls.length > 1) {
+      _initController(1).whenComplete(() => _lock = false);
+    }
   }
 
+  VoidCallback _listenerSpawner(index) {
+    return () {
+      int dur = _controller(index).value.duration.inMilliseconds;
+      int pos = _controller(index).value.position.inMilliseconds;
+      int buf = _controller(index).value.buffered.last.end.inMilliseconds;
+
+      setState(() {
+        if (dur <= pos) {
+          _position = 0;
+          return;
+        }
+        _position = pos / dur;
+        _buffer = buf / dur;
+      });
+      if (dur - pos < 1) {
+        if (index < _urls.length - 1) {
+          _nextVideo();
+        }
+      }
+    };
+  }
+
+  VideoPlayerController _controller(int index) {
+    return _controllers[_urls.elementAt(index)];
+  }
+
+  Future<void> _initController(int index) async {
+    var controller = VideoPlayerController.network(_urls.elementAt(index));
+    _controllers[_urls.elementAt(index)] = controller;
+    await controller.initialize();
+  }
+
+  void _removeController(int index) {
+    _controller(index).dispose();
+    _controllers.remove(_urls.elementAt(index));
+    _listeners.remove(index);
+  }
+
+  void _stopController(int index) {
+    _controller(index).removeListener(_listeners[index]);
+    _controller(index).pause();
+    _controller(index).seekTo(Duration(milliseconds: 0));
+  }
+
+  void _playController(int index) async {
+    if (!_listeners.keys.contains(index)) {
+      _listeners[index] = _listenerSpawner(index);
+    }
+    _controller(index).addListener(_listeners[index]);
+    await _controller(index).play();
+    setState(() {});
+  }
+
+  void _previousVideo() {
+    if (_lock || index == 0) {
+      return;
+    }
+    _lock = true;
+
+    _stopController(index);
+
+    if (index + 1 < _urls.length) {
+      _removeController(index + 1);
+    }
+
+    _playController(--index);
+
+    if (index == 0) {
+      _lock = false;
+    } else {
+      _initController(index - 1).whenComplete(() => _lock = false);
+    }
+  }
+
+  void _nextVideo() async {
+    if (_lock || index == _urls.length - 1) {
+      return;
+    }
+    _lock = true;
+
+    _stopController(index);
+
+    if (index - 1 >= 0) {
+      _removeController(index - 1);
+    }
+
+    _playController(++index);
+
+    if (index == _urls.length - 1) {
+      _lock = false;
+    } else {
+      _initController(index + 1).whenComplete(() => _lock = false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text("Playing ${index + 1} of ${_urls.length}"),
+        ),
+        body: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Expanded(
+              flex: 7,
+              child: Stack(children: [
+                GestureDetector(
+                  onLongPressStart: (_) => _controller(index).pause(),
+                  onLongPressEnd: (_) => _controller(index).play(),
+                  child: Center(
+                    child: AspectRatio(
+                      aspectRatio: _controller(index).value.aspectRatio,
+                      child: Center(child: VideoPlayer(_controller(index))),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    height: 10,
+                    width: MediaQuery.of(context).size.width * _buffer,
+                    color: Colors.grey,
+                  ),
+                ),
+                Positioned(
+                  child: Container(
+                    height: 10,
+                    width: MediaQuery.of(context).size.width * _position,
+                    color: Colors.greenAccent,
+                  ),
+                ),
+              ]),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              flex: 2,
+              child: Placeholder(),
             ),
+            SizedBox(height: 20),
+            Expanded(
+              flex: 2,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: _urls.length,
+                itemBuilder: (BuildContext context, int _index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: VideoItem(
+                      url: _urls.elementAt(_index),
+                      active: index == _index,
+                    ),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 20),
           ],
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('Done'),
+        ));
+  }
+}
+
+class VideoItem extends StatefulWidget {
+  VideoItem({Key key, this.url, this.active}) : super(key: key);
+
+  final String url;
+
+  final bool active;
+
+  @override
+  _VideoItemState createState() => _VideoItemState();
+}
+
+class _VideoItemState extends State<VideoItem> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(widget.url)
+      ..initialize().then((_) {
+        setState(() {}); //when your thumbnail will show.
+      });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: widget.active
+          ? BoxDecoration(
+              border: Border.all(width: 5), color: Colors.pinkAccent[400])
+          : BoxDecoration(),
+      child: VideoPlayer(
+        _controller,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
